86e95dd6c83234fcd16842e99ae8e15acaec2dc5
Anu409 <mothadianusha@gmail.com>
2025-06-05 16:23:05 +0530

    Add files via upload

---
+++ reversearray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class reversearray
+{
+	public static void main(String[] args) {
+		int a=8;
+		int b=3;
+		int temp=0;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a+" "+b);
+}
+}
