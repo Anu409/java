d82c6834e235afa2165b85d7a408a1a7bf5801b9
Anu409 <mothadianusha@gmail.com>
2025-06-06 15:57:53 +0530

    Add files via upload

---
+++ nonstaticadd.java
@@ -0,0 +1,14 @@
+public class nonstaticadd
+{
+	public static void add()
+	{
+	    int a=8;
+	    int b=8;
+	    int c=a+b;
+	    System.out.println(c);
+	}
+	public static void main(String[] args){
+	    main m=new main();
+	    m.add();
+	}
+	}
