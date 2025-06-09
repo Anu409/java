bcb3aef7c0075aac06344a5c5552fcce864d4420
Anu409 <mothadianusha@gmail.com>
2025-06-09 14:51:13 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,18 @@
+public class Main1
+{
+    public String fun(int n)
+    {
+        if(n==11) {
+            return"ankitha dharshini";
+        }
+        System.out.println(n);
+        String s=fun(n+1);
+        System.out.println(n);
+        return s;
+    }
+    public static void main(String[] args){
+        Main1 m=new Main1();
+        System.out.println(m.fun(1));
+    }
+    }
+    
